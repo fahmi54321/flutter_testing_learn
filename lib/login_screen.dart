a8461468e3daed5f255b72119e341ee114768c5b
unknown <fahmiabdulaziz44@gmail.com>
2022-11-10 08:58:01 +0700

    widget testing validation login

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_testing_learn/validator.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -13,25 +15,32 @@ class LoginScreen extends StatelessWidget {
         title: const Text('Login'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            TextFormField(
-              key: const ValueKey('email_field'),
-              controller: emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            const SizedBox(height: 20.0),
-            TextFormField(
-              key: const ValueKey('password_field'),
-              controller: passController,
-              decoration: const InputDecoration(labelText: 'Password'),
-            ),
-            const SizedBox(height: 30.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Simpan'),
-            ),
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                key: const ValueKey('email_field'),
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) => Validator.validateEmail(value ?? ""),
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                key: const ValueKey('password_field'),
+                controller: passController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                validator: (value) => Validator.validatePassword(value ?? ''),
+              ),
+              const SizedBox(height: 30.0),
+              ElevatedButton(
+                onPressed: () {
+                  formKey.currentState?.validate();
+                },
+                child: const Text('Simpan'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing_learn/login_screen.dart';
 
 void main() {
-  //todo 1
   testWidgets('Testing temukan widget title', (
     WidgetTester tester,
   ) async {
@@ -21,7 +20,6 @@ void main() {
     expect(title, findsOneWidget);
   });
 
-  //todo 2
   testWidgets('Testing temukan value key widget input email', (
     WidgetTester tester,
   ) async {
@@ -41,7 +39,6 @@ void main() {
     expect(keyEmail, findsOneWidget);
   });
 
-  //todo 3
   testWidgets('Testign temukan value key widget input password', (
     WidgetTester tester,
   ) async {
@@ -63,7 +60,6 @@ void main() {
     expect(keyPass, findsOneWidget);
   });
 
-  // todo 4 (finish)
   testWidgets('Testing temukan widget button', (WidgetTester tester) async {
     // Arrange
     await tester.pumpWidget(
@@ -77,5 +73,120 @@ void main() {
 
     // Assert
     expect(findButton, findsOneWidget);
+  });
+
+  //todo 1
+  testWidgets('Testing harus ada pesan required jika email dan password kosong',
+      (WidgetTester tester) async {
+    // Arrange
+    await tester.pumpWidget(
+      MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
+
+    // Act
+    Finder loginButton = find.byType(ElevatedButton); // temukan button
+    await tester.tap(loginButton); // ketika onTap
+    await tester
+        .pumpAndSettle(); // settle berarti mengatasi ketika klik onTap akan ada waktu yg dibutuhkan
+    Finder errorText = find.text('Required Field'); // temukan error text
+
+    // Assert
+    expect(errorText, findsNWidgets(2)); //  cek dua widget untuk error text
+  });
+
+  //todo 2
+  testWidgets('Testing email tidak valid', (WidgetTester tester) async {
+    // Arrange
+    await tester.pumpWidget(
+      MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
+
+    // Act
+
+    // temukan key input email
+    Finder keyEmail = find.byKey(
+      const ValueKey('email_field'),
+    );
+
+    // simulai input email salah
+    await tester.enterText(keyEmail, 'fahmigmail.com');
+
+    Finder loginButton = find.byType(ElevatedButton);
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle();
+
+    // pesan text ketika email tidak valid
+    Finder errorText = find.text('Please enter a valid email');
+
+    // Assert
+    expect(errorText, findsOneWidget);
+  });
+
+  // todo 3
+  testWidgets('Testing password tidak valid', (WidgetTester tester) async {
+    // Arrange
+    await tester.pumpWidget(
+      MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
+
+    // Act
+
+    // temukan key input password
+    Finder keyPass = find.byKey(
+      const ValueKey('password_field'),
+    );
+
+    // simulasi input password salah
+    await tester.enterText(keyPass, '123');
+
+    Finder loginButton = find.byType(ElevatedButton);
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle();
+
+    // pesan text ketika password tidak valid
+    Finder errorText = find.text('Password harus lebih dari 6 karakter');
+
+    // Assert
+    expect(errorText, findsOneWidget);
+  });
+
+  //todo 4 (finish)
+  testWidgets('Testign email dan password valid', (WidgetTester tester) async {
+    // Arrange
+    await tester.pumpWidget(
+      MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
+
+    // Act
+
+    // temukan key input
+    Finder keyEmail = find.byKey(
+      const ValueKey('email_field'),
+    );
+    Finder keyPass = find.byKey(
+      const ValueKey('password_field'),
+    );
+
+    // simulai input valid
+    await tester.enterText(keyEmail, 'fahmi@gmail.com');
+    await tester.enterText(keyPass, '123456789');
+
+    Finder loginButton = find.byType(ElevatedButton);
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle();
+
+    // pesan text input kosong
+    Finder errorText = find.text('Required Field');
+
+    // Assert
+    expect(errorText, findsNothing);
   });
 }
